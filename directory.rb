9f5a9a0b94f46cafd865a94512ed7b1284373e08
Sophie Gill <sophie-gill@hotmail.com>
2016-06-27 16:55:44 +0100

    exercise 8.4

--- directory.rb
+++ directory.rb
@@ -24,10 +24,10 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    if student[:name].length < 12
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
+  counter = 0
+  while counter < students.length do
+    puts "#{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+  counter += 1
   end
 end
 
