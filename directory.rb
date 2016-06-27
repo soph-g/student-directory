43563220d4e611b8c556c3c793f85833b5f3e71b
Sophie Gill <sophie-gill@hotmail.com>
2016-06-27 16:37:11 +0100

    exercise 8.1 each with index

--- directory.rb
+++ directory.rb
@@ -24,8 +24,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -34,6 +34,7 @@ def print_footer(students)
 end
 
 students = input_students
+# nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
