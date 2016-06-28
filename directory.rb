785a721e1bf0040cc455898bd550c43249ac0c84
Sophie Gill <sophie-gill@hotmail.com>
2016-06-28 15:19:58 +0100

    exercise 9

--- directory.rb
+++ directory.rb
@@ -1,11 +1,37 @@
 
+def interactive_menu
+  students = ""
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      # input the students
+    when "2"
+      print_header
+      print_students(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
-  name = gets.delete!("\n")
+  name = gets.chomp
   # while the name is empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
@@ -25,7 +51,7 @@ def print_header
   puts "---------------"
 end
 
-def print(students)
+def print_students(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
