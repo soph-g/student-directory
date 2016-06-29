24ca1d5bc3f1355287f113a8b9290be18db68dbd
Sophie Gill <sophie-gill@hotmail.com>
2016-06-29 12:27:51 +0100

    exercise 12)

--- directory.rb
+++ directory.rb
@@ -11,6 +11,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -22,6 +23,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -84,6 +87,15 @@ def save_students
   file.close
 end
 
+def load_students
+  @students = []
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
 
 
 # nothing happens until we call the methods
